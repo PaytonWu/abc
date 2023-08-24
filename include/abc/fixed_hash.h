4ff495f9185de703de0e4d77a139bbf792756051
payton.wu <payton.wu@outlook.com>
2023-08-24 23:56:55 +0800

    🎨 add fixed_hash.h, alias of fixed_bytes

---
+++ include/abc/fixed_hash.h
@@ -0,0 +1,17 @@
+// Copyright(c) 2023 - present, Payton Wu (payton.wu@outlook.com) & abc contributors.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#if !defined(ABC_INCLUDE_ABC_FIXED_HASH)
+#define ABC_INCLUDE_ABC_FIXED_HASH
+
+#pragma once
+
+#include <abc/fixed_bytes.h>
+
+namespace abc {
+
+using hash256_t = bytes32_t;
+
+}
+
+#endif //ABC_INCLUDE_ABC_FIXED_HASH
