2585f1e551b85702569696280b3b3e8bbacc9927
payton.wu <payton.wu@outlook.com>
2024-05-07 23:34:48 +0800

    ♻️ add hex_string_fwd_decl.h & hex_string_decl.h

---
+++ include/abc/hex_string_decl.h
@@ -0,0 +1,18 @@
+// Copyright(c) 2024 - present, Payton Wu (payton.wu@outlook.com) & the contributors.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#if !defined(ABC_INCLUDE_ABC_HEX_STRING_DECL)
+#define ABC_INCLUDE_ABC_HEX_STRING_DECL
+
+#pragma once
+
+#include "hex_string_fwd_decl.h"
+
+namespace abc
+{
+
+
+
+}
+
+#endif // ABC_INCLUDE_ABC_HEX_STRING_DECL
---
+++ include/abc/hex_string_fwd_decl.h
@@ -0,0 +1,16 @@
+// Copyright(c) 2024 - present, Payton Wu (payton.wu@outlook.com) & the contributors.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#if !defined(ABC_INCLUDE_ABC_HEX_STRING_FWD_DECL)
+#define ABC_INCLUDE_ABC_HEX_STRING_FWD_DECL
+
+#pragma once
+
+namespace abc
+{
+
+class [[nodiscard]] hex_string;
+
+}
+
+#endif // ABC_INCLUDE_ABC_HEX_STRING_FWD_DECL
